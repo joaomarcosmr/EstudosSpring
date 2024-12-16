427e80a8b32f962dee4361c50473259cad96a03c
João Marcos <joaomarcosmr4@gmail.com>
2024-12-16 15:18:54 -0300

    criei primeira rota

---
+++ src/main/java/dev/java10x/projetoapi/NinjaController.java
@@ -0,0 +1,15 @@
+package dev.java10x.projetoapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Boas-vindas - Olá mundo!";
+    }
+}
